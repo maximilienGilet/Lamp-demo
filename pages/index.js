d03fcc8abeca83e98c50af34911368845573c0be
Maximilien Gilet <maximilien248@gmail.com>
2020-07-31 17:37:08 +0200

    init NextJS + PWA

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,18 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charset='utf-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+        <meta name='description' content='Description' />
+        <meta name='keywords' content='Keywords' />
+        <title>Alad1 - gestion de votre lampe connectée</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#8a2be2"/>
       </Head>
 
       <main className={styles.main}>
